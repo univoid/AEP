294f09eb9871602dd75404e1fc91e2becdb9dc52
YuHui.Zheng <zyhlxw@gmail.com>
2015-12-18 19:30:02 +0900

    prototype of modelling module ver 2.0

--- modeling.py
+++ modeling.py
@@ -1,4 +1,5 @@
 from graph_tool.all import *
+from __future__ import division
 # constants
 # free velocity
 V_F = 0         # m / s
@@ -15,13 +16,27 @@ DELTA_T = 10    # s
 
 # define slope influence function
 def h(slope):
-    pass
+    if slope <= 5:
+        return 1
+    elif slope <= 10:
+        return 0.885
+    else:
+        return 0.75
+
 # define widening drop function
 def f(phi):
-    pass
+    return -0.005581 * phi + 0.9367
+
 # define widening drop function
-def g(w):
-    pass
+def g(u, edges):
+    w_in  = 0
+    w_out = 0
+    for e in edges:
+        if e.source() == u:
+            w_out += width[e]
+        else:
+            w_in += width[e]
+    return w_in / w_out
 
 # G: Original Graph
 G = load_graph("OrignalGraph.xml.gz")
@@ -30,15 +45,15 @@ G = load_graph("OrignalGraph.xml.gz")
 N = G.copy()
 
 # read vertex's properties
-type = N.vertex_properties["type"]
-phi = N.vertex_properties["phi"]
+type = N.vertex_properties["type"]      # widenning or bottleneck
+phi = N.vertex_properties["phi"]        # radians
 # read edges' properties
-length = N.edge_properties["length"]
-width = N.edge_properties["width"]
-slope = N.edge_properties["slope"]
-sat = N.edge_properties["sat"]
-B = N.graph_properties["B"]
-S = N.graph_properties["S"]
+length = N.edge_properties["length"]    # m
+width = N.edge_properties["width"]      # m
+slope = N.edge_properties["slope"]      # degrees
+sat = N.edge_properties["sat"]          # boolean
+B = N.graph_properties["B"]             # list
+S = N.graph_properties["S"]             # list
 # create new property maps
 cap = N.new_edge_property("int")
 tm = N.new_edge_property("int")
@@ -47,16 +62,16 @@ tm = N.new_edge_property("int")
 for e in G.vertices():
     if sat[e]:
         cap[e] = width[e] * RHO_OPT * DELTA_T
-        tm[e] = length[e] / (V_OPT * h(slope[e]) * DELTA_T)
+        tm[e] = length[e] // (V_OPT * h(slope[e]) * DELTA_T)
 
         u = e.source()
         if type(u) == "widenning":
             cap[e] = f(phi[u]) * cap[e]
         elif type(u) == "bottleneck":
-            cap[e] = g(u.all_edges()) * cap[e]
+            cap[e] = g(u, u.all_edges()) * cap[e]
     else:
         cap[e] = width[e] * RHO_M * DELTA_T
-        tm[e] = length[e] / (V_f * h(slope[e]) * DELTA_T)
+        tm[e] = length[e] // (V_F * h(slope[e]) * DELTA_T)
 
 # save new properties
 N.edge_properties["cap"] = cap
