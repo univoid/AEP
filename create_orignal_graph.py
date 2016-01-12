330e2ca91b82bbd3ea8d2471b7ece309a8efa338
YuHui.Zheng <zyhlxw@gmail.com>
2016-01-12 15:59:26 +0900

    Raw data completed

--- create_orignal_graph.py
+++ create_orignal_graph.py
@@ -14,64 +14,74 @@ with open("rawVertex.json", "r") as infile:
     for line in infile:
         data = loads(line)
         rawVList.append(data)
-
-
-# initialise Original Graph
-G = Graph()
-
-# create graph's properties
-# B = G.new_graph_property("object")      # list of departures
-# B[G] = []
-S = G.new_graph_property("object")      # list of refuges
-S[G] = []
-# create vertex's properties
-type = G.new_vertex_property("string")  # type of point
-phi = G.new_vertex_property("float")    # inner radian
-b = G.new_vertex_property("int")        # supply
-r = G.new_vertex_property("int")        # receive
-# create edge's properties
-length = G.new_edge_property("int")     # length of road
-width = G.new_edge_property("int")      # width of road
-slope = G.new_edge_property("int")      # slope of road
-sat = G.new_edge_property("int")        # saturation flag
-
-
-# input
-# graph properties
-S[G].extend(range(0, 8))
-
-# edge properties
-for item in rawEList:
-    e = G.add_edge(item["from"], item["to"])
-    sl = item["slope"]
-    slope[e] = int(sl) if sl else None
-    wi = item["width"]
-    width[e] = int(wi) if wi else None
-    le = item["length"]
-    length[e] = int(le) if le else None
-    sa = item["sat"]
-    sat[e] = sa
-    if item["duplex"] == "T":
-        e2 = G.add_edge(item["to"], item["from"])
-        slope[e2], width[e2], length[e2], sat[e2] =slope[e], width[e], length[e], sat[e]
-
-# vertex properties
+# the sum of evacuee
+sumb = 0
+# the sum of refuges' capacity
+sumr = 0
 for item in rawVList:
-    # TODO
-    pass
+    sumb += item["b"]
+    sumr += item["r"]
 
-# save properties
-# G.graph_properties["B"] = B
-G.graph_properties["S"] = S
-G.vertex_properties["type"] = type
-G.vertex_properties["phi"] = phi
-G.vertex_properties["b"] = b
-G.vertex_properties["r"] = r
-G.edge_properties["length"] = length
-G.edge_properties["width"] = width
-G.edge_properties["slope"] = slope
-G.edge_properties["sat"] = sat
+print "The sum of evacuee is {}, and the sum of refuges' capacity is {}".format(sumb, sumr)
 
-# save Original Graph
-G.save("OrignalGraph.xml.gz")
-G.save("OrignalGraph.xml")
+
+
+# # initialise Original Graph
+# G = Graph()
+#
+# # create graph's properties
+# # B = G.new_graph_property("object")      # list of departures
+# # B[G] = []
+# S = G.new_graph_property("object")      # list of refuges
+# S[G] = []
+# # create vertex's properties
+# type = G.new_vertex_property("string")  # type of point
+# phi = G.new_vertex_property("float")    # inner radian
+# b = G.new_vertex_property("int")        # supply
+# r = G.new_vertex_property("int")        # receive
+# # create edge's properties
+# length = G.new_edge_property("int")     # length of road
+# width = G.new_edge_property("int")      # width of road
+# slope = G.new_edge_property("int")      # slope of road
+# sat = G.new_edge_property("int")        # saturation flag
+#
+#
+# # input
+# # graph properties
+# S[G].extend(range(0, 8))
+#
+# # edge properties
+# for item in rawEList:
+#     e = G.add_edge(item["from"], item["to"])
+#     sl = item["slope"]
+#     slope[e] = int(sl) if sl else None
+#     wi = item["width"]
+#     width[e] = int(wi) if wi else None
+#     le = item["length"]
+#     length[e] = int(le) if le else None
+#     sa = item["sat"]
+#     sat[e] = sa
+#     if item["duplex"] == "T":
+#         e2 = G.add_edge(item["to"], item["from"])
+#         slope[e2], width[e2], length[e2], sat[e2] =slope[e], width[e], length[e], sat[e]
+#
+# # vertex properties
+# for item in rawVList:
+#     # TODO
+#     pass
+#
+# # save properties
+# # G.graph_properties["B"] = B
+# G.graph_properties["S"] = S
+# G.vertex_properties["type"] = type
+# G.vertex_properties["phi"] = phi
+# G.vertex_properties["b"] = b
+# G.vertex_properties["r"] = r
+# G.edge_properties["length"] = length
+# G.edge_properties["width"] = width
+# G.edge_properties["slope"] = slope
+# G.edge_properties["sat"] = sat
+#
+# # save Original Graph
+# G.save("OrignalGraph.xml.gz")
+# G.save("OrignalGraph.xml")
--- getRaw.py
+++ getRaw.py
@@ -8,8 +8,24 @@ with open("rawEdge.json", "w") as outFile:
     for st in l:
         stt = '{' + st + '}'
         stt = stt.replace('{', '{\'').replace(':', '\':').replace(', ', ', \'')
-        temp = eval(stt)
-        dump(temp, outFile)
+        data = eval(stt)
+        # parsing type
+        slope = data["slope"]
+        data["slope"] = int(slope) if slope else 0
+        fr = data["from"]
+        data["from"] = int(fr) if fr else None
+        to = data["to"]
+        data["to"] = int(to) if to else None
+        width = data["width"]
+        data["width"] = float(width) if width else None
+        length = data["length"]
+        data["length"] = float(length) if length else None
+        duplex = data["duplex"]
+        data["duplex"] = True if (duplex == "T") else False
+        sat = data["sat"]
+        data["sat"] = True if (sat == "T") else False
+        # save
+        dump(data, outFile)
         outFile.write('\n')
 
 # Vertex input
@@ -20,6 +36,18 @@ with open("rawVertex.json", "w") as outFile:
     for st in l:
         stt = '{' + st + '}'
         stt = stt.replace('{', '{\'').replace(':', '\':').replace(', ', ', \'')
-        temp = eval(stt)
-        dump(temp, outFile)
+        data = eval(stt)
+        # parsing type
+        type = data["type"]
+        data["type"] = type if type else None
+        b = data["b"]
+        data["b"] = int(b) if b else 0
+        r = data["r"]
+        data["r"] = int(r) if r else 0
+        num = data["num"]
+        data["num"] = int(num) if num else None
+        phi = data["phi"]
+        data["phi"] = float(phi) if phi else 0
+        # save
+        dump(data, outFile)
         outFile.write('\n')
